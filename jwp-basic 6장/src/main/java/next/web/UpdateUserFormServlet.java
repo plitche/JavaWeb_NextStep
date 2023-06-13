176b35dcf3b5a3cb38f3281d98e4b411468bb915
plitche <kysu728@naver.com>
2023-06-13 23:01:24 +0900

    6.1.2 개인정보수정

--- jwp-basic 6장/src/main/java/next/web/UpdateUserFormServlet.java
+++ jwp-basic 6장/src/main/java/next/web/UpdateUserFormServlet.java
@@ -1,6 +1,9 @@
 package next.web;
 
 import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,11 +16,31 @@ import java.io.IOException;
 @WebServlet("/user/update")
 public class UpdateUserFormServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserFormServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute("user", DataBase.findUserById("plitche"));
         RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
         rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+
+        User user = DataBase.findUserById(userId);
+        if (password.equals(user.getPassword())) {
+            user = new User(userId, password, name, email);
+            log.debug("user : {}", user);
+
+            DataBase.addUser(user);
+            resp.sendRedirect("/user/list");
+        } else {
+            resp.sendRedirect("/user/update");
+        }
     }
 }
