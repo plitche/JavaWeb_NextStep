cdc2e11faf2649bc53b3929c2f364b767f80fd0e
plitche <kysu728@naver.com>
2023-06-15 23:49:39 +0900

    6.1.3 로그인/로그아웃 기능 실습

---
+++ jwp-basic 6장/src/main/java/next/web/LogOutServlet.java
@@ -0,0 +1,29 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/logout")
+public class LogOutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(LogOutServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("user");
+        RequestDispatcher rd = req.getRequestDispatcher("/");
+        rd.forward(req, resp);
+    }
+}
---
+++ jwp-basic 6장/src/main/java/next/web/LoginServlet.java
@@ -0,0 +1,37 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(LoginServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        log.debug("userId : {}", userId);
+
+        User user = DataBase.findUserById(userId);
+
+        HttpSession session = req.getSession();
+        if (user.getPassword().equals(password)) {
+            session.setAttribute("user", user);
+        }
+        RequestDispatcher rd = req.getRequestDispatcher("/");
+        rd.forward(req, resp);
+    }
+}
