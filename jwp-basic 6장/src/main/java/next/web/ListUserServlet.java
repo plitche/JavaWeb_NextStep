18c436f1c9271b259a023e609b57950111708093
plitche <kysu728@naver.com>
2023-06-18 18:59:42 +0900

    6.1.4 회원 목록 및 개인정보 수정 보안 강화 실습

--- jwp-basic 6장/src/main/java/next/web/ListUserServlet.java
+++ jwp-basic 6장/src/main/java/next/web/ListUserServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import core.db.DataBase;
 
@@ -17,8 +18,14 @@ public class ListUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("users", DataBase.findAll());
-        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-        rd.forward(req, resp);
+        HttpSession session = req.getSession();
+        Object user = session.getAttribute("user");
+        if (user != null) {
+            req.setAttribute("users", DataBase.findAll());
+            RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+            rd.forward(req, resp);
+        } else {
+            resp.sendRedirect("/");
+        }
     }
 }
