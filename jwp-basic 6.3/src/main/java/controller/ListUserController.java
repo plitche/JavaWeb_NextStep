fccdb1fe1407cda690bba60fa311567b726690b5
plitche <kysu728@naver.com>
2023-06-21 23:04:46 +0900

    6.3.4 클라이언트별 세션 저장소 추가

---
+++ jwp-basic 6.3/src/main/java/controller/ListUserController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import db.DataBase;
+import http.HttpRequest;
+import http.HttpResponse;
+import http.HttpSession;
+import model.User;
+
+import java.util.Collection;
+
+public class ListUserController extends AbstractController {
+
+    @Override
+    public void doGet(HttpRequest request, HttpResponse response) {
+        if (!isLogined(request.getSession())) {
+            response.sendRedirect("/user/login.html");
+            return;
+        }
+
+        Collection<User> users = DataBase.findAll();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table border='1'>");
+        for (User user : users) {
+            sb.append("<tr>");
+            sb.append("<td>" + user.getUserId() + "</td>");
+            sb.append("<td>" + user.getName() + "</td>");
+            sb.append("<td>" + user.getEmail() + "</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        response.forwardBody(sb.toString());
+    }
+
+    private static boolean isLogined(HttpSession session) {
+        Object user = session.getAttribute("user");
+        if (user == null) {
+            return false;
+        }
+        return true;
+    }
+}
