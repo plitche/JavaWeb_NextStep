b06dac06da8172f37e646a0071c9ffd7c63e2013
plitche <kysu728@naver.com>
2023-06-25 23:47:21 +0900

    6.5 MVC 프레임워크 구현 1단계

--- jwp-basic 6.3/src/main/java/core/mvc/DispatcherServlet.java
+++ jwp-basic 6.3/src/main/java/core/mvc/DispatcherServlet.java
@@ -4,15 +4,51 @@ package core.mvc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialversionUID = 1L;
-    private static final Logger loggger = LoggerFactory.getLogger(DispatcherServlet.class);
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     private RequestMapping rm;
+
+    @Override
+    public void init() throws ServletException {
+        rm = new RequestMapping();
+        rm.initMapping();
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+        String requestUri = request.getRequestURI();
+        logger.debug("Method : {}, Request URI : {}", request.getMethod(), requestUri);
+
+        Controller controller = rm.findController(requestUri);
+        try {
+            String viewName = controller.execute(request, response);
+            move(viewName, request, response);
+        } catch (Throwable e) {
+            logger.error("Exception : {}", e);
+            throw new ServletException(e.getMessage());
+        }
+    }
+
+    protected void move(String viewName, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher(viewName);
+        rd.forward(request, response);
+    }
 
 }
