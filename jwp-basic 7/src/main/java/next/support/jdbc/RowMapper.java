0574705c5b43ec9b9ec2d1aac1a618fdd54f66ce
plitche <kysu728@naver.com>
2023-07-12 23:15:48 +0900

    7.5 데이터베이스, 디자인 패턴

--- jwp-basic 7/src/main/java/next/support/jdbc/RowMapper.java
+++ jwp-basic 7/src/main/java/next/support/jdbc/RowMapper.java
@@ -3,6 +3,7 @@ package next.support.jdbc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface // 이 인터페이스는 아래 메서드 하나만을 가진다. 함수처럼 사용 가능
 public interface RowMapper<T> {
     T mapRow(ResultSet rs) throws SQLException;
 }
